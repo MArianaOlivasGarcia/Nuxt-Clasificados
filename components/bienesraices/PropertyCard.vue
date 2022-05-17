b9ab04d0a0a8f3e8dfa3894aa0a1e12987b67bdc
MAriana Olivas <olivasgarcia031096@gmail.com>
2022-05-16 22:30:28 -0500

    paginator

--- components/bienesraices/PropertyCard.vue
+++ components/bienesraices/PropertyCard.vue
@@ -49,7 +49,7 @@ export default {
         }
     },
      created() {
-         console.log(this.property)
+        //  console.log(this.property)
 
         this.route = `/bienesraices/propiedad/${helpers.normalize( this.property.name )}_${ this.property.productoid }.html`
     },
