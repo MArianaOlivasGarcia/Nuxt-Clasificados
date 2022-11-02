c4b36f659b6dcfc1214317d740e211857061fc8f
MAriana Olivas <olivasgarcia031096@gmail.com>
2022-11-02 13:26:48 -0500

    nav

--- pages/bienesraices/propiedad/_id.vue
+++ pages/bienesraices/propiedad/_id.vue
@@ -665,7 +665,7 @@ data() {
             { end: 576, size: 1 },
             { start: 576, end: 768, size: 2 },
             { start: 768, end: 992, size: 3 },
-            { size: 3 },
+            { size: 4 },
             ],
             list: {
             // 1200 because @media (min-width: 1200px) and therefore I want to switch to windowed mode
