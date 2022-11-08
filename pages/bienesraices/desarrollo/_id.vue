afc7213d6d1de11a3021a41f1b02ae23b12b6ac7
MAriana Olivas <olivasgarcia031096@gmail.com>
2022-11-08 17:28:47 -0500

    prop

--- pages/bienesraices/desarrollo/_id.vue
+++ pages/bienesraices/desarrollo/_id.vue
@@ -348,39 +348,52 @@
 
           <div class="mt-3" v-if="property.prototipos.length > 0">
 
-            <div class="card protoCard mb-2" v-for="p in property.prototipos" :key="p.prototypeid">
-              <div class="row m-0">
+            <div class="protContainer">
 
-                <div class="col-md-2 p-0 d-flex justify-content-center align-items-center my-1">
-                  <div style="width: 70px; height: 70px;">
-                    <img  
-                    class="protoImage"
-                    :src=" 'https://clasificadoscontacto.com'+ p.image"
-                    alt="">
+              <p style="font-weight: bold; color: grey;">En venta / <span style="color: black;">{{ property.prototipos.length }} unidades</span></p>
+
+              <div class="card protoCard mb-2" v-for="p in property.prototipos" :key="p.prototypeid">
+                
+                <div class="row m-0">
+
+                  <div class="col-md-4 p-0 d-flex justify-content-center align-items-center">
+                      <img  
+                      class="protoImage"
+                      :src=" 'https://clasificadoscontacto.com'+ p.image"
+                      alt="">
                   </div>
+
+                  <div class="col-md-8 d-flex flex-column justify-content-center ">
+                    <p style="font-weight: bold;">{{ p.name }}</p>
+                    <!-- <p class="m-0 card-text text-muted">{{ property.colonia }}, {{ property.municipio }}, {{ property.state }}</p> -->
+                    <p class="m-0 price" v-if="p.price">Venta ${{ Number(p.price).toLocaleString()  }} {{ p.currency }}</p>
+                    <p class="m-0 price" v-if="p.pricer">Renta ${{ Number(p.pricer).toLocaleString()  }} {{ p.currencyr }}</p>
+                    <p class="m-0 price" v-if="!p.price">Consultar el precio</p>
+                    <p class="m-0 price" v-if="!p.pricer">Consultar el precio</p>
+                  </div>
+
+                
+
+
                 </div>
 
-                <div class="col-md-4 d-flex flex-column justify-content-center p-0">
-                  <p class="m-0">{{ p.name }}</p>
-                  <!-- <p class="m-0 card-text text-muted">{{ property.colonia }}, {{ property.municipio }}, {{ property.state }}</p> -->
-                  <p class="m-0 price" v-if="p.price">Venta ${{ Number(p.price).toLocaleString()  }} {{ p.currency }}</p>
-                  <p class="m-0 price" v-if="p.pricer">Renta ${{ Number(p.pricer).toLocaleString()  }} {{ p.currencyr }}</p>
-                  <p class="m-0 price" v-if="!p.price">Consultar el precio</p>
-                  <p class="m-0 price" v-if="!p.pricer">Consultar el precio</p>
-                </div>
 
-                <div class="col-md-6 d-flex justify-content-center align-items-center p-0">
+                <div class="row m-0 m-3">
+                  <div class="col-md-12 d-flex justify-content-center align-items-center p-0">
 
-                  <span v-if="p.bedrooms > 0"><i class="icon-big-bed-with-one-pillow pl-2 pr-1"></i>{{ p.bedrooms }}</span>
-                  <span v-if="p.bathrooms > 0"><i class="icon-bath pl-2 pr-1"></i>{{ p.bathrooms }}</span>
-                  <span v-if="p.m2c > 0"><i class="icon-ruler pl-2 pr-1"></i>{{ p.m2c }} m<sup>2</sup></span>
-                  <span v-if="p.mlot > 0"><i class="icon-text  pl-2 pr-1"></i>{{ p.mlot }} m<sup>2</sup></span>
+                    <span v-if="p.bedrooms > 0"><i class="icon-big-bed-with-one-pillow pl-2 pr-1"></i>{{ p.bedrooms }}</span>
+                    <span v-if="p.bathrooms > 0"><i class="icon-bath pl-2 pr-1"></i>{{ p.bathrooms }}</span>
+                    <span v-if="p.m2c > 0"><i class="icon-ruler pl-2 pr-1"></i>{{ p.m2c }} m<sup>2</sup></span>
+                    <span v-if="p.mlot > 0"><i class="icon-text  pl-2 pr-1"></i>{{ p.mlot }} m<sup>2</sup></span>
                           
+                  </div>
                 </div>
 
 
               </div>
+
             </div>
+
 
           </div>
 
@@ -716,11 +729,16 @@ font-size: 35px;
 }
 
 .protoImage {
-  width: 70px !important;
-  height: 70px !important;
+  width: 100% !important;
 }
 
 .protoCard p, .protoCard span,  .protoCard i{
-  font-size: 12px;
+  font-size: 14px;
 }
+
+.protContainer {
+  border: 1px solid #e3e6e7;
+  padding: 10px 10px;
+}
+
 </style>
