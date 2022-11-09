f6a4e1d9ac688a316916132e84864e20cce4560a
MAriana Olivas <olivasgarcia031096@gmail.com>
2022-11-09 12:21:15 -0500

    css

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -114,7 +114,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
   .image {
     width: 100%;
     height: 300px;
--- components/GalleryHorizontal.vue
+++ components/GalleryHorizontal.vue
@@ -96,7 +96,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
   .image {
     width: 100%;
     height: 200px;
